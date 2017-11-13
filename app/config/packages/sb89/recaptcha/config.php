fb18595cc32928230b158dd7b82a369da518dbca
Selva Kumar <selvakumar.a@zcodia.com>
2017-11-13 16:03:00 +0530

    Captcha keys updated

--- app/config/packages/sb89/recaptcha/config.php
+++ app/config/packages/sb89/recaptcha/config.php
@@ -1,6 +1,6 @@
 <?php
     return array(
-        'public_key'=>'6LdbSQ8UAAAAAPfrPGR7I-hejOl3oJ5Hd6_RFjgD',
-        'private_key'=>'6LdbSQ8UAAAAACErNd5cT19n40TdKvxVa68OJi3M'
+        'public_key'=>'6LexyjUUAAAAABHmjpKiiCihU86wrZ_PvEIEK2qo',
+        'private_key'=>'6LexyjUUAAAAACqA9yT568gRoS6ryQoQDGOV2314'
     );
 ?>
