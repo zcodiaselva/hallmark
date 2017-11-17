0684ba15eec6c576112c9d81d9186cb877dbd41f
Selva Kumar <selvakumar.a@zcodia.com>
2017-11-17 16:07:26 +0530

    Hyperlink to Agent Login

--- app/views/site/_partials/right_menu.blade.php
+++ app/views/site/_partials/right_menu.blade.php
@@ -1,10 +1,11 @@
  
  <ul id="top-menu" class="nav navbar-nav main-nav right">
+	<li><a href="http://commercial.hallmarkgrp.com/Login.asp" target="_blank">Agent Login</a></li>
     @foreach($menu as $product)
     @if($product->order > 8 )  
-		<li><a href="{{ URL::to($product->slug_en)}}">{{ $product->name_en }}</a></li>
+		<li><a href="{{ URL::to($product->slug_en)}}">{{ $product->name_en }}</a></li>	
     @else
-		
+			
     @endif
-    @endforeach
+    @endforeach	
 </ul>
